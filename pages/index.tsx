a12329b5ae18601a6a3eb5588a40ca694933372e
psemeghin <pedrohmsemeghini@gmail.com>
2025-08-08 23:07:42 +0200

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center text-center px-4">
+      <h1 className="text-4xl font-bold mb-4">Improve Center</h1>
+      <p className="text-xl mb-2">Wellness. Fitness. Healthness.</p>
+      <p className="mb-4 text-lg">Unlock your next level.</p>
+      <p className="text-sm text-gray-700">Science and nature on your side.</p>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        ocean: '#004C57',
+        horizon: '#CCE9F2'
+      }
+    }
+  },
+  plugins: [],
+}
