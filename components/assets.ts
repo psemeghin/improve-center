efd32a230a2bf83f3566f81c6c405212cc351891
psemeghin <pedrohmsemeghini@gmail.com>
2025-08-12 18:48:40 +0200

    Create assets.ts

---
+++ components/assets.ts
@@ -0,0 +1,9 @@
+// components/assets.ts
+export const IMAGES = {
+  logoLight: "/media/logos/LogoHorizontal-AzulOceanoProfundo.svg", // para fundo claro
+  logoDark:  "/media/logos/LogoHorizontal-Branco.svg",             // para fundo escuro
+  heroLight: "/media/banners/og-default-v1.png",
+  heroDark:  "/media/banners/og-default-dark-v1.png",
+  ogImage:   "/media/banners/og-default-v1.png",
+  icon512:   "/media/icons/Simbolo-Preto.svg", // se quiser usar como ilustração
+};
