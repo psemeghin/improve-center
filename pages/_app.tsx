814ae6659e485d5d3584a30018c7f8a6537ec83d
psemeghin <pedrohmsemeghini@gmail.com>
2025-08-15 16:57:27 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,6 @@
-// pages/_app.tsx
 import type { AppProps } from "next/app";
-import Layout from "@/components/Layout";
-import "@/styles/globals.css";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  return <Component {...pageProps} />;
 }
